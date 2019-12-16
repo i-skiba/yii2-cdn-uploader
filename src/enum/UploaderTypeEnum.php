5d20ff7ba3dc80d2cc958608fdf093a04597eb5e
Olzhas Kulzhambekov <exgamer@live.ru>
2019-12-16 16:03:49 +0600

    - local-file delete in progress

---
+++ src/enum/UploaderTypeEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace kamaelkz\yii2cdnuploader\enum;
+
+use concepture\yii2logic\enum\Enum;
+
+/**
+ * Class UploaderTypeEnum
+ * @package kamaelkz\yii2cdnuploader\enum
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class UploaderTypeEnum extends Enum
+{
+    const CDN = 1;
+    const LOCAL = 2;
+}
